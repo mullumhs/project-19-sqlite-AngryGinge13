016ef5c94794d6cb36cefa964a7b664a77250bf2
AngryGinge13 <Sonny.mcgauley@education.nsw.gov.au>
2025-11-11 09:49:29 +1100

    manageed folders, added classes and edit file(not working)

--- app.py
+++ app.py
@@ -1,64 +1,54 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-import sqlite3
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(app)
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
 
 @app.route('/add', methods=['GET', 'POST'])
 
 def add_movie():
-    # On a form submission (POST)
-
     if request.method == 'POST':
-
-        title = request.form['title']
-
-        director = request.form['director']
-
-        year = int(request.form['year'])
-
-        rating = float(request.form['rating'])
-
-        
-
-        conn = get_db_connection()
-
-        conn.execute('INSERT INTO movies (title, director, year, rating) VALUES (?, ?, ?, ?)',
-
-                     (title, director, year, rating))
-
-        conn.commit()
-
-        conn.close()
-
+        new_movie = Movie(
+            title=request.form['title'],
+            director=request.form['director'],
+            year=int(request.form['year']),
+            rating=float(request.form['rating'])
+        )
+        db.session.add(new_movie)
+        db.session.commit()
         return redirect(url_for('index'))
-
-    
-    # On visiting the page (GET)
-
     return render_template('add.html')
 
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
 
-def get_db_connection():
+def edit_movie(id):
 
-    conn = sqlite3.connect('movieinterface.db')
+    movie = Movie.query.get_or_404(id)
 
-    return conn
-
-
-
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    conn.row_factory = sqlite3.Row
-    movies = conn.execute('SELECT * FROM movies').fetchall()
-    conn.close()
-    return render_template('index.html', movies=movies)
-
+    if request.method == 'POST':
+        movie.title = request.form['title']
+        movie.director = request.form['director']
+        movie.year = int(request.form['year'])
+        movie.rating = float(request.form['rating'])
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
 
 if __name__ == '__main__':
-
     app.run(debug=True)
